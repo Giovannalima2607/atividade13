4dd1d86049a2e5d92d90c5873dbe2c22df2dc6ec
Giovannalima2607 <giovannasilva123@gmail.com>
2024-09-13 11:43:53 -0300

    positivo ou negativo

---
+++ atividade 13.py
@@ -0,0 +1,4 @@
+# Crie um programa que receba um número do usuário e informe se ele é positivo, negativo ou zero.
+#slicita ao usuario digitar um numero inteiro
+numero = int(input('digite um numero inteiro:'))
+print(numero, 'e um numero,' 'positivo.'if numero >= 0 else 'negativo.')
